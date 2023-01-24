5b6057dd1018c2392a007150c95d13f8a9034eea
Gaston-Diaz <gastondiaz.lp@gmail.com>
2023-01-24 20:25:59 -0300

    Se agrego el routerlink para las paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntregaProductosComponent } from './components/entrega-productos/entrega-productos.component';
+import { EntregaTonersComponent } from './components/entrega-toners/entrega-toners.component';
+import { PcsComponent } from './components/pcs/pcs.component';
+import { PerifericosComponent } from './components/perifericos/perifericos.component';
+import { TonersComponent } from './components/toners/toners.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'toners',component: TonersComponent},
+  {path:'entregaToners',component: EntregaTonersComponent},
+  {path:'pcs',component:PcsComponent},
+  {path:'perisfericos',component: PerifericosComponent},
+  {path: 'entregaPerisfericos', component: EntregaProductosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
