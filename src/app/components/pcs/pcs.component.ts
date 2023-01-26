7a06d5d2c9d59f93f5339da1c746a3ea4c0f7529
Gaston <gastondiaz.lp@gmail.com>
2023-01-26 14:02:48 -0300

    Se trabajo sobre el formulario de las pcs

--- src/app/components/pcs/pcs.component.ts
+++ src/app/components/pcs/pcs.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Pc } from 'src/app/models/pc.model';
 
 @Component({
   selector: 'app-pcs',
   templateUrl: './pcs.component.html',
   styleUrls: ['./pcs.component.css']
 })
-export class PcsComponent implements OnInit {
+export class PcsComponent {
+  titulo = "Lista de PCs"
 
   constructor() { }
 
-  ngOnInit(): void {
+  pcs:Pc[]=[];
+
+  agregarPC(){
+    let miPc = new Pc(this.inputId,this.inputRam,this.inputSO,this.inputSSD,this.inputExtra,this.inputDisponible);
+    this.pcs.push(miPc);
   }
 
+  inputId: string="";
+  inputRam: string="";
+  inputSO: string="";
+  inputSSD: string="";
+  inputExtra: string="";
+  inputDisponible: string="SI";
 }
