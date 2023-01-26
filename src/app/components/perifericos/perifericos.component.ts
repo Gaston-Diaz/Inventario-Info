e14419ecf18d5227833ff7d66e0f5132ecc6fbce
Gaston <gastondiaz.lp@gmail.com>
2023-01-26 12:36:30 -0300

    Se trabajo sobre el formulario de los perisfericos

--- src/app/components/perifericos/perifericos.component.ts
+++ src/app/components/perifericos/perifericos.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Periferico } from 'src/app/models/periferico.model';
 
 @Component({
   selector: 'app-perifericos',
   templateUrl: './perifericos.component.html',
   styleUrls: ['./perifericos.component.css']
 })
-export class PerifericosComponent implements OnInit {
+export class PerifericosComponent {
+  titulo = "Lista de Perisfericos"
 
   constructor() { }
 
-  ngOnInit(): void {
+  perisfericos:Periferico[]=[
+
+  ]
+
+  agregarPerisferico(){
+    let miPerisferico = new Periferico(this.inputNombreP,this.inputDescripcion,this.inputObservacion,this.inputStock);
+    this.perisfericos.push(miPerisferico);
   }
+
+  inputNombreP:string="";
+  inputDescripcion:string="";
+  inputObservacion:string="OK";
+  inputStock:number=0;
 
 }
