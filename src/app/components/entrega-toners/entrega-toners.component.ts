e9c42146f804fae5f760f38352bc06be4af26f9d
Gaston <gastondiaz.lp@gmail.com>
2023-01-26 15:56:55 -0300

    Se trabajo sobre el componente entrega de toners

--- src/app/components/entrega-toners/entrega-toners.component.ts
+++ src/app/components/entrega-toners/entrega-toners.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { EToner} from 'src/app/models/eToner.model'
+
 
 @Component({
   selector: 'app-entrega-toners',
   templateUrl: './entrega-toners.component.html',
   styleUrls: ['./entrega-toners.component.css']
 })
-export class EntregaTonersComponent implements OnInit {
+export class EntregaTonersComponent {
+  titulo = "Entrega de Toners";
 
   constructor() { }
 
-  ngOnInit(): void {
+  etoners:EToner[]=[]
+
+  agregarEToner(){
+    let eToner = new EToner(this.inputFecha,this.inputIdToner,this.inputColor,this.inputEntregadoA,this.inputOficina,this.inputDe);
+    this.etoners.push(eToner);
+
   }
 
+  inputFecha:string= "";
+  inputIdToner:string= "";
+  inputColor:string= "";
+  inputEntregadoA:string= "";
+  inputOficina:string= "";
+  inputDe:string = "";
 }
