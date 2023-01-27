3a4909db0523bce4665c1f48f3de61649b699631
Gaston <gastondiaz.lp@gmail.com>
2023-01-27 09:45:14 -0300

    Se trabajo sobre los componentes de entrega de pc

--- src/app/components/entrega-pc/entrega-pc.component.ts
+++ src/app/components/entrega-pc/entrega-pc.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { EPC } from 'src/app/models/ePc-model';
+
 
 @Component({
   selector: 'app-entrega-pc',
   templateUrl: './entrega-pc.component.html',
   styleUrls: ['./entrega-pc.component.css']
 })
-export class EntregaPcComponent implements OnInit {
+export class EntregaPcComponent{
+  titulo = "Entrega de PCs"
 
   constructor() { }
 
-  ngOnInit(): void {
+  ePcs: EPC[]=[];
+  
+  agregarEPC(){
+    let ePc = new EPC(this.inputFechaPC,this.inputIdPc,this.inputEntregadoA,this.inputOficina,this.inputDe);
+    this.ePcs.push(ePc);
   }
 
+  inputFechaPC:string= "";
+  inputIdPc:string = "";
+  inputEntregadoA:string = "";
+  inputOficina:string = "";
+  inputDe:string = ""
+ 
 }
--- src/app/models/ePc-model.ts
+++ src/app/models/ePc-model.ts
@@ -1,11 +1,13 @@
 export class EPC{
-    constructor(unId:string,unaEntrega:string,unaOfi:string,de:string){
+    constructor(unaFecha:string,unId:string,unaEntrega:string,unaOfi:string,de:string){
+        this.fechaEPC = unaFecha;
         this.idPc = unId;
         this.entregadoA = unaEntrega;
         this.oficina = unaOfi;
         this.de = de;
     }
 
+    fechaEPC:string ="";
     idPc:string = "";
     entregadoA:string = "";
     oficina:string = "";
