0ec2ed16c0380a15b75d9bd3b19a4277a421a200
Gaston <gastondiaz.lp@gmail.com>
2023-01-27 10:36:55 -0300

    Se trabajo sobre el componente entrega de perisfericos y se corrigo el texto por defecto de los demas componentes

--- src/app/components/entrega-productos/entrega-productos.component.ts
+++ src/app/components/entrega-productos/entrega-productos.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { EPerisferico } from 'src/app/models/ePerisferico.model';
 
 @Component({
   selector: 'app-entrega-productos',
   templateUrl: './entrega-productos.component.html',
   styleUrls: ['./entrega-productos.component.css']
 })
-export class EntregaProductosComponent implements OnInit {
+export class EntregaProductosComponent {
+  titulo= "Entrega de Perifericos"
 
   constructor() { }
 
-  ngOnInit(): void {
+  ePerifericos: EPerisferico[]=[]
+
+  agregarEPeriferico(){
+    let ePeriferico = new EPerisferico(this.inputFecha,this.inputProducto,this.inputDescripcion,this.inputCantidad,this.inputEntregadoA,this.inputOficina,this.inputDe);
+    this.ePerifericos.push(ePeriferico);
   }
+
+  inputFecha:string="";
+  inputProducto:string="";
+  inputDescripcion:string="";
+  inputCantidad:number=0;
+  inputEntregadoA:string="";
+  inputOficina:string="";
+  inputDe:string="";
 
 }
